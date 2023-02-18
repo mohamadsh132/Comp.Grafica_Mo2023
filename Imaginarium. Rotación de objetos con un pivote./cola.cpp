c56d7795cacbbf7170e46120adf451983e5c4024
mohamadsh132 <85518656+mohamadsh132@users.noreply.github.com>
2023-02-18 17:57:49 -0500

    ultimo arreglo

--- Imaginarium. Rotación de objetos con un pivote./cola.cpp
+++ Imaginarium. Rotación de objetos con un pivote./cola.cpp
@@ -1,50 +1,60 @@
 #include "Cola.h"
 
-void Cola::DibujarCola(float p, int n, float x, float y, float z)
+void Cola::DibujarCola(float Angulo, int n, float x, float y, float z)
 {
 	glPushMatrix();
-	glTranslatef(x, y, z);
+	  glTranslatef(x, y, z);
 
-	//Aqui reutilizado y modificado de: http://www.glprogramming.com/red/chapter03.html
+	  //Aqui reutilizado y modificado de: http://www.glprogramming.com/red/chapter03.html
 
-	glPushMatrix();
-	glTranslatef(0.30, 0.0, -1);
-	glRotatef(p, 0.0, 0.0, 1.0); //angulo=45, define cuanto rota 
-	glTranslatef(-0.25, 2, -2);
+	  glPushMatrix();
+	  glTranslatef(-0.25, 0.0, 0.0);
+	  glRotatef(Angulo, 0.0, 0.0, 1.0); //angulo=45, define cuanto rota 
+	  glTranslatef(0.25, 0.0, 0.0);
 
-	glPushMatrix();
-	glScalef(0.5, 0.1, 0.5); 
-	glutSolidCube(0.8);
-	glPopMatrix();
-	float r = 0.2;
-	float q = 0.6;
+	  glPushMatrix();
+	  glScalef(0.5, 0.4, 1.0);
+	  glutSolidCube(1.0);
+	  glPopMatrix();
 
-	for (int i = 0; i < 3; i++) {
-		glTranslatef(0.10, 0.0, 0);
-		glRotatef(p, 0.0, 0.0, -1.0);
-		glTranslatef(0.25, 0.0, 0);
-		glPushMatrix();
-		glScalef(0.5, r, q);
-		glutSolidCube(0.6);
-		glPopMatrix();
-		r += 0.2;
-		q += 0.1;
-	}
 
-	for (int i = 0; i < (n - 3); i++) {  #En esta parte me ayudaron :)
 
-		glTranslatef(0.10, 0.0, 0.0);
-		glRotatef(p, 0.0, 0.0, -1.0);
+	  for (int i = 0; i < n; i++) {
+
+		glTranslatef(0.25, 0.0, 0.0);
+		glRotatef(Angulo, 0.0, 0.0, 1.0);
 		glTranslatef(0.25, 0.0, 0.0);
 		glPushMatrix();
 		glScalef(0.5, 0.4, 1.0);
-		glutSolidCube(0.7);
+		glutSolidCube(1.0);
 		glPopMatrix();
-		p = p - 0.9;
-	}
 
+	  }
 
-	glPopMatrix();
+	  for (int i = 0; i < 10; i++) {
+
+		  glTranslatef(0.15, 0.0, 0.0);
+		  glRotatef(Angulo, 0.0, 0.0, 0.5);
+		  glTranslatef(0.15, 0.0, 0.0);
+		  glPushMatrix();
+		  glScalef(0.5, 0.4, 1.0);
+		  glutSolidCube(0.7);
+		  glPopMatrix();
+
+	  }
+	  for (int i = 0; i < 5; i++) {
+
+		  glTranslatef(0.1, 0.0, 0.0);
+		  glRotatef(Angulo, 0.0, 0.0, 0.5);
+		  glTranslatef(0.1, 0.0, 0.0);
+		  glPushMatrix();
+		  glScalef(0.5, 0.4, 1.0);
+		  glutSolidCube(0.7);
+		  glPopMatrix();
+
+	  }
+	  glPopMatrix();
+
 
 	glPopMatrix();
 }
